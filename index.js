681b6133aafe2f935ac410c54b0eb9b4f007c05e
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-07 20:55:36 +0000

    connecting the app to mongoose

--- index.js
+++ index.js
@@ -19,3 +19,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
