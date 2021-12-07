144d11e10ad24ff3a33a1861e2f93cda343a63fb
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-07 21:32:30 +0000

    adding " get task by id " functionnality

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -2,3 +2,18 @@ var express = require("express");
 var router = express.Router();
 var task = require("../models/taskModel");
 const tasks = require("../controllers/taskController");
+
+router.route("/:id").get(async (req, res, next) => {
+  try {
+    const taskById = await task.findById(req.params.id);
+    if (!taskById) {
+      return res.status(404).json({ msg: "" });
+    }
+    res.status(200).json({
+      status: "success",
+      data: taskById,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
