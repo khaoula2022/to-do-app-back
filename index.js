73e940e105ef666a2e0080efb72d239f6853f222
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-07 20:53:33 +0000

    adding the required code to establish the connection with mongoose

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ const app = express();
 const server = http.createServer(app);
 const bodyParser = require("body-parser");
 const path = require("path");
+
+const CONNECTION_URL =
+  "mongodb+srv://internship:internship@cluster0.hqnas.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
