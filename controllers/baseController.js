f1ec5b608f604957737e25115b8008fb0219f337
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-07 21:41:07 +0000

    adding getTaskById method to the baseController

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -11,3 +11,25 @@ exports.getAll = (Model) => async (req, res, next) => {
     next(error);
   }
 };
+
+exports.getOne = (Model) => async (req, res, next) => {
+  try {
+    const doc = await Model.findById(req.params.id);
+
+    if (!doc) {
+      return next(
+        new AppError(404, "fail", "Please try to enter valid data"),
+        req,
+        res,
+        next
+      );
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: doc,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
