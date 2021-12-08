5455d5d96873cc1b989dccf2d61ffd598ea97f6d
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-08 01:26:16 +0000

    adding the password-hashing function

--- models/userModel.js
+++ models/userModel.js
@@ -42,5 +42,13 @@ userSchema.methods.correctPassword = async function (
   return await bcrypt.compare(typedPassword, originalPassword);
 };
 
+userSchema.pre("save", async function (next) {
+  
+  if (!this.isModified("password")) {
+    return next();
+  }
+ this.password = await bcrypt.hash(this.password, 12);
+ next();
+});
 const User = mongoose.model("User", userSchema);
 module.exports = User;
