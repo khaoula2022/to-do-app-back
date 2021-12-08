bffc759829fd3c13d33310a42ff56d4563976d6b
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-08 00:19:04 +0000

    adding the "required" aspect to the username field

--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
+    required: [true, "Please fill your username"],
   },
   email: {
     type: String,
